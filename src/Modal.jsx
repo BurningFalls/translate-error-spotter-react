a396d5634504589ac0c4b63d1535855996ae1c35
BurningFalls <sherlocky31415@gmail.com>
2023-12-08 00:13:19 +0900

    feat: modal에 pie chart 추가

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Modal.css'
+import { ResponsivePie } from '@nivo/pie'
 import {
   MDBBtn,
   MDBModal,
@@ -16,6 +17,7 @@ export default function Modal() {
   const [textInput, setTextInput] = useState('');
   const [responseData, setResponseData] = useState([]);
   const [selectedLanguage, setSelectedLanguage] = useState('한글'); // 기본 언어 선택
+  const [isLoadData, setLoadData] = useState(false);
 
   const toggleOpen = () => setBasicModal(!basicModal);
 
@@ -42,6 +44,7 @@ export default function Modal() {
 
       if (response.ok) {
         const data = await response.json();
+        setLoadData(true);
         setResponseData(data);
       } else {
         console.error('API request failed');
@@ -51,10 +54,156 @@ export default function Modal() {
     }
   };
 
+  const colors = [
+    'hsl(262, 70%, 50%)',
+    'hsl(188, 70%, 50%)',
+    'hsl(343, 70%, 50%)',
+    'hsl(48, 70%, 50%)',
+    // 원하는 만큼 색상을 추가할 수 있습니다.
+  ];
+
+  const myData = responseData.map((item, index) => ({
+    id: item.label,
+    label: item.label,
+    value: new Intl.NumberFormat('en-US', { maximumFractionDigits: 2 }).format(item.score * 100),
+    color: colors[index % colors.length], // 인덱스에 따라 색상을 선택
+  }));
+
+  const MyResponsivePie = ({ myData /* see data tab */ }) => (
+    <ResponsivePie
+      data={myData}
+      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+      innerRadius={0.5}
+      padAngle={0.7}
+      cornerRadius={3}
+      activeOuterRadiusOffset={8}
+      borderWidth={1}
+      borderColor={{
+        from: 'color',
+        modifiers: [
+          [
+            'darker',
+            0.2
+          ]
+        ]
+      }}
+      arcLinkLabelsSkipAngle={10}
+      arcLinkLabelsTextColor="#333333"
+      arcLinkLabelsThickness={2}
+      arcLinkLabelsColor={{ from: 'color' }}
+      arcLabelsSkipAngle={10}
+      arcLabelsTextColor={{
+        from: 'color',
+        modifiers: [
+          [
+            'darker',
+            '2'
+          ]
+        ]
+      }}
+      defs={[
+        {
+          id: 'dots',
+          type: 'patternDots',
+          background: 'inherit',
+          color: 'rgba(255, 255, 255, 0.3)',
+          size: 4,
+          padding: 1,
+          stagger: true
+        },
+        {
+          id: 'lines',
+          type: 'patternLines',
+          background: 'inherit',
+          color: 'rgba(255, 255, 255, 0.3)',
+          rotation: -45,
+          lineWidth: 6,
+          spacing: 10
+        }
+      ]}
+      fill={[
+        {
+          match: {
+            id: 'ruby'
+          },
+          id: 'dots'
+        },
+        {
+          match: {
+            id: 'c'
+          },
+          id: 'dots'
+        },
+        {
+          match: {
+            id: 'go'
+          },
+          id: 'dots'
+        },
+        {
+          match: {
+            id: 'python'
+          },
+          id: 'dots'
+        },
+        {
+          match: {
+            id: 'scala'
+          },
+          id: 'lines'
+        },
+        {
+          match: {
+            id: 'lisp'
+          },
+          id: 'lines'
+        },
+        {
+          match: {
+            id: 'elixir'
+          },
+          id: 'lines'
+        },
+        {
+          match: {
+            id: 'javascript'
+          },
+          id: 'lines'
+        }
+      ]}
+      legends={[
+        {
+          anchor: 'bottom',
+          direction: 'row',
+          justify: false,
+          translateX: 30,
+          translateY: 56,
+          itemsSpacing: 0,
+          itemWidth: 80,
+          itemHeight: 18,
+          itemTextColor: '#999',
+          itemDirection: 'left-to-right',
+          itemOpacity: 1,
+          symbolSize: 18,
+          symbolShape: 'circle',
+          effects: [
+            {
+              on: 'hover',
+              style: {
+                itemTextColor: '#000'
+              }
+            }
+          ]
+        }
+      ]}
+    />
+  )
+
+
   return (
-    <>
+    <div className="bert-button">
       <MDBBtn size='lg' outline onClick={toggleOpen}>BERT</MDBBtn>
-      <MDBModal open={basicModal} setopen={setBasicModal} tabIndex='-1'>
+      <MDBModal staticBackdrop open={basicModal} setopen={setBasicModal} tabIndex='-1'>
         <MDBModalDialog>
           <MDBModalContent>
               <MDBModalHeader className="modal-header-container">
@@ -71,6 +220,7 @@ export default function Modal() {
                 <MDBBtn className='btn-close' color='none' onClick={toggleOpen}></MDBBtn>
               </MDBModalHeader>
             <MDBModalBody>
+
               <MDBInput
                 label='Text input'
                 id='typeText'
@@ -78,17 +228,28 @@ export default function Modal() {
                 value={textInput}
                 autocomplete="off"
                 onChange={(e) => setTextInput(e.target.value)}
+                onClick={() => setLoadData(false)}
               />
               <p></p>
-              <div>
-                <ul>
-                  {responseData.map((item, index) => (
-                    <li key={index}>
-                      <strong>{item.label}:</strong> {new Intl.NumberFormat('en-US', { maximumFractionDigits: 2 }).format(item.score * 100)}%
-                    </li>
-                  ))}
-                </ul>
-              </div>
+
+              {isLoadData && (
+                <div className="result-data">
+                  {/*<div className="result-number">*/}
+                  {/*  <ul>*/}
+                  {/*    {responseData.map((item, index) => (*/}
+                  {/*      <li key={index}>*/}
+                  {/*        <strong>{item.label}:</strong> {new Intl.NumberFormat('en-US', { maximumFractionDigits: 2 }).format(item.score * 100)}%*/}
+                  {/*      </li>*/}
+                  {/*    ))}*/}
+                  {/*  </ul>*/}
+                  {/*</div>*/}
+
+                  <div className="result-chart">
+                    <MyResponsivePie myData={myData} />
+                  </div>
+                </div>
+              )}
+
             </MDBModalBody>
 
             <MDBModalFooter>
@@ -100,6 +261,6 @@ export default function Modal() {
           </MDBModalContent>
         </MDBModalDialog>
       </MDBModal>
-    </>
+    </div>
   );
 }
